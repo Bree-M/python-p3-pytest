7b56d7575f000ec58d2110015544901735d9c590
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-05-13 03:33:03 +0300

    commit my test

--- lib/string_functions.py
+++ lib/string_functions.py
@@ -1,6 +1,7 @@
 def return_string():
-    return ''
+      return ''
 
-def interpolate_string(s):
-    return f'Hello, {s}!'
+def interpolate_welcome(name):
+      return f'Hello, {name}!'
+    
     
--- lib/testing/test_string.py
+++ lib/testing/test_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from string_functions import return_string, interpolate_string
+from string_functions import return_string, interpolate_welcome
 
 def test_return_string():
     '''in string_functions, function "return_string()" returns a variable of type str.'''
@@ -8,4 +8,4 @@ def test_return_string():
 
 def test_interpolate_string():
     '''in string_functions, function "interpolate_string()" takes a string and inserts it into another string.'''
-    assert interpolate_string('Guido') == 'Hello, Guido!'
+    assert interpolate_welcome('Guido') == 'Hello, Guido!'
